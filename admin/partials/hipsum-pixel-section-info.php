035fd982b636d3a5c2346722500363643f38f56d
Laith Sinawi <laith@laithsinawi.com>
2016-11-23 16:39:49 -0800

    make text edits

--- admin/partials/hipsum-pixel-section-info.php
+++ admin/partials/hipsum-pixel-section-info.php
@@ -1,4 +1,5 @@
 <h1>Hipsum Pixel Settings</h1>
-<p>On page and post editor toolbar, you'll find the LoremPixel button. The button open a complete UI where you generate your content, preview it, and then Inert into Post.</p>
+<p>On page and post editor toolbar, you'll find the HipsumPixel button. The button opens a complete UI where you generate your content, preview it, and then Inert into Post.</p>
+<h3>Editor Toolbar Screenshot</h3>
 <img src="<?php echo plugin_dir_url(__DIR__) . 'img/screenshot-1.png';  ?>" alt="Hipsum Pixel button in editor window">
 <p>If you find this plugin useful, please <a href="https://wordpress.org/support/plugin/hipsum-pixel/reviews/">post a review</a>.  It can be only a few words if you want.  Thanks!</p>
